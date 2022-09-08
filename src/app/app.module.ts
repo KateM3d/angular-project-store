938cc880536f341e235b42b20737220b7bdd0adf
KateM3d <ekaterina8medvedeva@gmail.com>
2022-09-08 16:14:23 -0400

    add modal window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
 import { FormsModule } from '@angular/forms';
 import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { ModalComponent } from './components/modal/modal.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, GlobalErrorComponent, FilterProductsPipe],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    GlobalErrorComponent,
+    FilterProductsPipe,
+    ModalComponent,
+    CreateProductComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit {
+  @Input() title: string | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
